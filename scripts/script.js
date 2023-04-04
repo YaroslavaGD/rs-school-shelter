b1ef0dac6cbe243b81ec3f81bf6a2fd55d54df9c
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-04-04 22:38:29 +0200

    feat: implement css for mobile-menu

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const page = document.querySelector('.page');
+const navButton = document.querySelector('.nav-button');
+const nav = document.querySelector('.nav');
+const navItems = [... document.querySelectorAll('.nav__item')];
+
+console.log(navButton);

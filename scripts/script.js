0b4cdbbffe991e851ee5327c9f4dcc7a947d0c5e
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-04-05 21:49:37 +0200

    feat: implement js for mobile-menu

--- scripts/script.js
+++ scripts/script.js
@@ -3,4 +3,40 @@ const navButton = document.querySelector('.nav-button');
 const nav = document.querySelector('.nav');
 const navItems = [... document.querySelectorAll('.nav__item')];
 
-console.log(navButton);
+const toggleMenu = () => {
+  const isOpen = navButton.classList.contains('nav-button--open');
+  navButton.classList.toggle('nav-button--open');
+  nav.classList.toggle('nav--open');
+  page.classList.toggle('page--clip');
+}
+
+navButton.addEventListener('click', (e) => {
+  e.stopPropagation();
+
+  toggleMenu();
+});
+
+navItems.forEach(element => {
+  element.addEventListener('click', e => {
+    toggleMenu(); 
+  });
+});
+
+document.addEventListener('click', e => {
+  const target = e.target;
+  const isNav = target == nav || nav.contains(target);
+  const isNavButton = target == navButton;
+  const isNavOpen = nav.classList.contains('nav--open');
+
+  if (!isNav && !isNavButton && isNavOpen) {
+    toggleMenu();
+  }
+});
+
+window.addEventListener('resize', function() {
+  if (document.documentElement.clientWidth > 767) {
+    navButton.classList.remove('nav-button--open');
+    page.classList.remove('page--clip');
+    nav.classList.remove('nav--open');
+  }
+}, false);
